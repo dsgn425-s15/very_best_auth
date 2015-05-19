f8616e171edc8c25e0b513d08eb2932ddbbb4152
Raghu Betina <rbetina@github.com>
2015-05-19 15:30:02 -0500

    Adds list of favorites to neighborhoods#show

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,5 @@ class Neighborhood < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => { :scope => :city }
 
   has_many :venues
+  has_many :favorites, :through => :venues
 end
