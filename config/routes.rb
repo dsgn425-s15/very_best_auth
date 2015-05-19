cc67c59701761771b0555eaa453922d8d7c209c4
Raghu Betina <rbetina@github.com>
2015-05-19 14:56:43 -0500

    Generates User resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the User resource:
+  # CREATE
+  get "/users/new", :controller => "users", :action => "new"
+  post "/create_user", :controller => "users", :action => "create"
+
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
+  # UPDATE
+  get "/users/:id/edit", :controller => "users", :action => "edit"
+  post "/update_user/:id", :controller => "users", :action => "update"
+
+  # DELETE
+  get "/delete_user/:id", :controller => "users", :action => "destroy"
+  #------------------------------
+
   # Routes for the Cuisine resource:
   # CREATE
   get "/cuisines/new", :controller => "cuisines", :action => "new"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519195608) do
+ActiveRecord::Schema.define(version: 20150519195627) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20150519195608) do
     t.integer  "dish_id"
     t.integer  "venue_id"
     t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
