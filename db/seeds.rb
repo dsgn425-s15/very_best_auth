59e12019c73178d24db8ad9548cb3307e383750d
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-26 23:20:36 -0500

    ready for devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -127,6 +127,9 @@ usernames = ["abby", "alex", "mike", "raghu"]
 usernames.each do |username|
   user = User.new
   user.username = username
+  user.email = "#{username}@example.com"
+  user.password = "12341234"
+  user.password_confirmation = "12341234"
   user.save
 end
 
